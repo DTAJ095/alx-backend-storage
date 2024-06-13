70ba84b4c1b7c5b9224ef570fe925f48e2a9cdab
DTAJ095 <albanjaures26@gmail.com>
2024-06-13 22:26:35 +0100

    added

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- Script should fail if table already exists
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL
 );
