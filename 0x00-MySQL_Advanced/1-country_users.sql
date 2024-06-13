b614d2dd76f3c81931759a83d685482358abb162
DTAJ095 <albanjaures26@gmail.com>
2024-06-13 22:28:59 +0100

    updated

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Create table users
 -- Script should fail if table already exists
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
