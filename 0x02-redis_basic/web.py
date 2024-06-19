148138d406efd9c70aeaf00fc1a0dc41fadd6dfb
DTAJ095 <albanjaures26@gmail.com>
2024-06-19 23:29:17 +0100

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,17 +6,17 @@ from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
+def track_get_page(func: Callable) -> Callable:
     """ get_page decorator """
 
-    @wraps(fn)
+    @wraps(func)
     def wrapper(url: str) -> str:
         """ wrapper function """
         redis.Redis().incr(f'count:{url}')
         cached_page = redis.Redis().get(f'{url}')
         if cached_page:
             return cached_page.decode('utf-8')
-        response = fn(url)
+        response = func(url)
         redis.Redis().set(f'{url}', response, 10)
         return response
     return wrapper
